fe1de06deda5d2887b0bee98816612e9488f7474
Rumman Ahmed <ahmed.rumman@gmail.com>
2025-10-24 14:09:24 +1100

    chore: allow configuring mongo database name

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     MONGO_URI: str = "mongodb://mongo:27017/ai_matching"
+    MONGO_DB_NAME: str = "ai_matching"
     APP_NAME: str = "AI Matching Job API"
     DEBUG: bool = True
 
--- app/core/database.py
+++ app/core/database.py
@@ -2,4 +2,4 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.core.config import settings
 
 client = AsyncIOMotorClient(settings.MONGO_URI)
-db = client.get_database("ai_matching")
+db = client.get_database(settings.MONGO_DB_NAME)
